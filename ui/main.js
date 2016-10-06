cab94c7992b2707914a18b7617dc1a04e955f0a0
enileda <adeline.andrews@gmail.com>
2016-10-06 23:08:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,20 @@
 var button = document.getElementById('counter');
 var counter =0;
 button.onclick = function() {
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechanger = function() {
+        if (request.readystate == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
+    
+    request.open('Get',"http://enileda.imad.hasura-app.io/counter", true);
+    request.send(null);
 }
 
 
