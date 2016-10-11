49c499dc2d64469bed0c5c17d439b75d44a2fbc2
enileda <adeline.andrews@gmail.com>
2016-10-12 00:24:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,8 @@ button.onclick = function() {
     
     request.onreadystatechange = function() {
         if (request.readystate == XMLHttpRequest.DONE) {
-            if (request.status == 200) {
+            //if (request.status == 200) 
+            {
                 var counter = request.responseText;
                 counter = counter + 1;
                 var span = document.getElementById('count');
