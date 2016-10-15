b94952a325ae7715bf8e4ce07b35e6132bff5b20
enileda <adeline.andrews@gmail.com>
2016-10-15 10:25:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/adeline.html', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) {
+app.get('/submit-name.html', function (req, res) {
     var name = req.query.name;
 
     names.push(name);
